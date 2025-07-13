18fee8ce29ac8fb6029aa6d9f2b350a3d99e0d54
kazurayam <kazuaki.urayama@gmail.com>
2025-07-13 15:23:45 +0900

    use the MonkDirectoryScanner

--- Include/scripts/groovy/com/kazurayam/ks/importtestcasesintoexcel/ZipperTest.groovy
+++ Include/scripts/groovy/com/kazurayam/ks/importtestcasesintoexcel/ZipperTest.groovy
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertNotEquals
 import static org.junit.Assert.assertTrue
 import static org.junit.Assert.fail
 
+import org.junit.After
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -13,16 +16,41 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
+import org.apache.commons.io.FileUtils
+
 @RunWith(JUnit4.class)
 public class ZipperTest {
 	
+	@BeforeClass
+	static void beforeAll() {
+		Path testOutput = Paths.get("build/tmp/testOutput")
+		if (Files.exists(testOutput)) {
+			FileUtils.deleteDirectory(testOutput.toFile())
+		}
+	}
+	
+	@Before
+	void setup() {
+		println "---------"
+	}
+	
 	@Test
-	void test_smoke() {
-		String tcId = "TC1"
-		String xlsx = "build/tmp/testOutput/out.xlsx"
+	void test_All() {
+		String xlsx = "build/tmp/testOutput/test_All.xlsx"
 		Path xlsxPath = Paths.get(xlsx)
-		Zipper zipper = new Zipper(tcId, xlsx)
+		Zipper zipper = new Zipper(xlsx)
 		zipper.execute()
 		assertTrue(Files.exists(xlsxPath))
 	}
+	
+	@Test
+	void test_foo() {
+		String xlsx = "build/tmp/testOutput/test_foo.xlsx"
+		Path xlsxPath = Paths.get(xlsx)
+		String[] includes = [ "foo/**/*.groovy" ]
+		Zipper zipper = new Zipper(includes, xlsx)
+		zipper.execute()
+		assertTrue(Files.exists(xlsxPath))
+	}
+	
 }
--- Keywords/com/kazurayam/ks/importtestcasesintoexcel/Zipper.groovy
+++ Keywords/com/kazurayam/ks/importtestcasesintoexcel/Zipper.groovy
@@ -1,63 +1,81 @@
 package com.kazurayam.ks.importtestcasesintoexcel
 
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static org.assertj.core.api.InstanceOfAssertFactories.THROWABLE
-
-import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.hssf.usermodel.HSSFSheet
-import org.apache.poi.hssf.usermodel.HSSFRow
-import org.apache.poi.hssf.usermodel.HSSFCell
-import org.apache.poi.hssf.usermodel.HSSFRichTextString
-
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.nio.charset.StandardCharsets
+
+import org.apache.poi.hssf.usermodel.HSSFCell
+import org.apache.poi.hssf.usermodel.HSSFRow
+import org.apache.poi.hssf.usermodel.HSSFSheet
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+
+import com.kazurayam.ant.DirectoryScanner
 
 public class Zipper {
 
-	private String testCaseId
+	private String baseDir = "Scripts"
+	
+	private String[] includes
+	
+	private String xlsx
 
-	private String excelFilePath
-
-	Zipper(String testCaseId, String excelFilePath) {
-		this.testCaseId = testCaseId
-		this.excelFilePath = excelFilePath
+	Zipper(String xlsx) {
+		this(["**/*.groovy"], xlsx)
+	}
+	
+	Zipper(String[] includes, String xlsx) {		
+		this.includes = includes
+		this.xlsx = xlsx
 	}
 
 	void execute() {
-		Path testCaseScript = Paths.get(findTestCase(testCaseId).getGroovyScriptPath())
-		if (!Files.exists(testCaseScript)) {
-			throw new IOException(testCaseScript + " is not present")
+		HSSFWorkbook workbook = new HSSFWorkbook()
+		//
+		DirectoryScanner ds = new DirectoryScanner()
+		ds.setBasedir(this.baseDir)
+		ds.setIncludes(this.includes)
+		ds.scan()
+		String[] groovyFiles = ds.getIncludedFiles()
+		println "baseDir: " + this.baseDir
+		println "includes: " + this.includes
+		println "number of files: " + groovyFiles.length
+		for (int i = 0; i < groovyFiles.length; i++) {
+			Path p = Paths.get(baseDir).resolve(Paths.get(groovyFiles[i]))
+			println p
+			HSSFSheet spreadSheet = workbook.createSheet(resolveTestCaseId(p))
+			importScriptIntoSheet(p, spreadSheet)
 		}
+		//
+		writeWorkbookIntoFile(workbook, xlsx)
+	}
+
+	private void importScriptIntoSheet(Path p, HSSFSheet spreadSheet) {
+		BufferedReader br = 
+			new BufferedReader(new InputStreamReader(p.newInputStream(), StandardCharsets.UTF_8))
+		String line
+		int ln = 0
+		while((line = br.readLine()) != null) {
+			HSSFRow row = spreadSheet.createRow((short) ln)
+			HSSFCell cell0 = row.createCell(0)
+			cell0.setCellValue(line)
+			ln++
+		}
+	}
+
+	private void writeWorkbookIntoFile(HSSFWorkbook workbook, String excelFilePath) {
+		ByteArrayOutputStream baOut = new ByteArrayOutputStream()
+		workbook.write(baOut)
+		//
 		Path xlsx = Paths.get(excelFilePath)
 		Files.createDirectories(xlsx.getParent())
-		
-		//
-		HSSFWorkbook workBook = new HSSFWorkbook()
-		
-		//
-		HSSFSheet spreadSheet = workBook.createSheet(testCaseId)
-		
-		// 
-		HSSFRow row = spreadSheet.createRow((short) 0)
-		
-		//
-		HSSFCell cell0 = row.createCell(0)
-		cell0.setCellValue(new HSSFRichTextString("Hello"))
-		
-		HSSFCell cell1 = row.createCell(1)
-		cell1.setCellValue(new HSSFRichTextString("World"))
-		
-		//
-		FileOutputStream fout = new FileOutputStream(xlsx.toFile())
-		
-		//
-		ByteArrayOutputStream baOut = new ByteArrayOutputStream()
-		workBook.write(baOut)
-
 		FileOutputStream fOut = new FileOutputStream(xlsx.toFile())
 		baOut.writeTo(fOut)
 		baOut.close()
 		fOut.close()
+	}
+	
+	private String resolveTestCaseId(Path groovyFile) {
+		return groovyFile.getParent().toString().replaceAll("Scripts/","").replaceAll("/","／")
 	}
 }
